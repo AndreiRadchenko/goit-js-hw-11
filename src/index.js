5eb3202841bf6833d900fafe6b390f10c7be1d7f
AndreiRadchenko <d4100kx@gmail.com>
2022-10-09 01:33:53 +0300

    hw11 done

--- src/index.js
+++ src/index.js
@@ -1,29 +1,109 @@
-import fetchImages from './js/fetchimages';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import FetchImagesService from './js/fetchimages';
 import SimpleLightbox from 'simplelightbox';
 import galleryTemplate from './templates/gallery.hbs';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const NOTIFY_TIMEOUT = 5000; //ms
+let isSubmitPressed = false;
+
+const fetchImagesService = new FetchImagesService();
+
 const searchFormRef = document.querySelector('#search-form');
 const galleryRef = document.querySelector('.gallery');
+const sentinelRef = document.querySelector('#sentinel');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionClass: 'custom-caption',
+});
 
 searchFormRef.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
+  isSubmitPressed = true;
   event.preventDefault();
-  galleryRef.innerHTML = '';
-  const searchQuery = event.currentTarget.elements.searchQuery;
-  fetchImages(searchQuery.value)
+  clearGallery();
+  const inputQuery = event.currentTarget.elements.searchQuery.value.trim();
+  fetchImagesService.query = inputQuery;
+  fetchImagesService.resetPage();
+  if (inputQuery === '') {
+    return;
+  }
+  fetchImagesService
+    .fetchImages()
+    .then(onFetchResult)
     .then(renderGallery)
     .catch(error => console.log(error));
 }
 
+async function onFetchResult(fetchResult) {
+  if (fetchResult.total === 0) {
+    Notify.warning(
+      `Sorry, there are no images matching your search query. Please try again.`,
+      {
+        timeout: NOTIFY_TIMEOUT,
+      }
+    );
+    throw new Error('There are no images matching search query');
+  }
+  if (fetchImagesService.page === 1 && fetchResult.total !== 0) {
+    Notify.success(`Hooray! We found ${fetchImagesService.total} images`, {
+      timeout: NOTIFY_TIMEOUT,
+    });
+  } else if (fetchImagesService.fetchedImages <= fetchImagesService.total) {
+    Notify.success(
+      `Fetched ${fetchImagesService.fetchedImages} images from ${fetchImagesService.total} total found`,
+      {
+        timeout: NOTIFY_TIMEOUT,
+      }
+    );
+  }
+
+  return fetchResult.hits;
+}
+
 function renderGallery(fetchResult) {
   galleryRef.insertAdjacentHTML('beforeend', galleryTemplate(fetchResult));
+  fetchImagesService.incrementPage();
+  isSubmitPressed = false;
 
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    captionClass: 'custom-caption',
-  });
+  lightbox.refresh();
 }
+
+function clearGallery() {
+  galleryRef.innerHTML = '';
+}
+
+const onEntry = entries => {
+  entries.forEach(entry => {
+    if (
+      entry.isIntersecting &&
+      !isSubmitPressed &&
+      fetchImagesService.query !== ''
+    ) {
+      if (fetchImagesService.fetchedImages === fetchImagesService.total) {
+        Notify.info(
+          `We're sorry, but you've reached the end of search results.`,
+          {
+            timeout: NOTIFY_TIMEOUT,
+          }
+        );
+      } else if (fetchImagesService.fetchedImages < fetchImagesService.total) {
+        fetchImagesService
+          .fetchImages()
+          .then(onFetchResult)
+          .then(renderGallery)
+          .catch(error => console.log(error));
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onEntry, {
+  rootMargin: '150px',
+});
+
+observer.observe(sentinelRef);
--- src/js/fetchimages.js
+++ src/js/fetchimages.js
@@ -1,22 +1,53 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const RESPONSE_OK = 200;
-
-const searchParams = {
-  key: '30410400-df54a4fa47e0d802e49478434',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: 'true',
-  page: 1,
-  per_page: 9,
-};
-
-export default async function fetchImages(name) {
-  searchParams.q = name;
-  const response = await axios.get(BASE_URL, { params: searchParams });
-  if (response.status !== RESPONSE_OK) {
-    throw new Error(response.status);
+export default class FetchImagesService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.total = 0;
+    this.fetchedImages = 0;
   }
-  return response.data.hits;
+
+  BASE_URL = 'https://pixabay.com/api/';
+  RESPONSE_OK = 200;
+
+  searchParams = {
+    key: '30410400-df54a4fa47e0d802e49478434',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: 1,
+    per_page: 40,
+  };
+
+  async fetchImages() {
+    const { searchParams, BASE_URL, RESPONSE_OK } = this;
+    searchParams.q = this.query;
+    searchParams.page = this.page;
+    const response = await axios.get(BASE_URL, { params: searchParams });
+    if (response.status !== RESPONSE_OK) {
+      throw new Error(response.status);
+    }
+    this.total = response.data.total;
+    this.fetchedImages += response.data.hits.length;
+    return response.data;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+    this.fetchedImages = 0;
+    this.total = 0;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
